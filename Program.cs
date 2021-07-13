438da5aafa1679e08c77d859a15db8f91e6d0edb
Daniel Okello <o.daniel@student.ciu.ac.ug>
2021-07-13 07:59:37 +0300

    Level Points Challenge

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+using System;
+
+namespace _33_Code_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int levels = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Points(levels));
+        }
+
+        static int Points(int levels){
+          if(levels < 1){
+              return 0;
+          }else if (levels == 1){
+            return 1;
+          }
+          else
+          {
+              return levels + Points(levels - 1);
+          }
+        }
+    }
+}
